4323d571ae91273fb384b85af54a2876ac29a67f
Abdurahmon <omadbekmobdas01@gmail.com>
2021-12-21 03:00:13 +0500

    First commit :)

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QFont font = ui->label->font();
+    font.setPixelSize(20);
+    ui->label->setFont(font);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
